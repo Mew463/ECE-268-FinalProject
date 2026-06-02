15a251309b6228c9504958da2fe9acb13b99b70d
Mew463 <72902803+Mew463@users.noreply.github.com>
2026-06-02 14:24:38 -0700

    running

--- gpu_implementation.cu
+++ gpu_implementation.cu
@@ -193,8 +193,8 @@ int main() {
         // bignum p = 9461917253336215331;
         // bignum q = 13954742674334932447;
 
-        bignum p = 7;
-        bignum q = 13;
+        bignum p = 13;
+        bignum q = 131;
 
         RSAKeyPair keys = generate_keys(p, q, e);
         bignum d = keys.private_key.exponent;
