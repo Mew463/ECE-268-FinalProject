2aecd7eb8467c96b8920a27e24a640adc0a796b6
Mew463 <72902803+Mew463@users.noreply.github.com>
2026-06-07 10:34:24 -0700

    running

--- gpu_implementation.cu
+++ gpu_implementation.cu
@@ -349,7 +349,7 @@ int main() {
         cudaMemcpy(d_input, original_message, size_message * sizeof(char), cudaMemcpyHostToDevice);
                 
         // Run Kernel
-        int numThreads = 128;
+        int numThreads = 256;
         int numBlocks = 1;
 
 
